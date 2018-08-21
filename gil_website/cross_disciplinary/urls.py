4a582230ad9b2255245f7598a12eca23100d14a4
dachenlian <cclemon.lian@gmail.com>
2018-08-22 01:05:15 +0800

    added requirements, course_info, student_area, and purpose to cross_disciplinary

--- gil_website/cross_disciplinary/urls.py
+++ gil_website/cross_disciplinary/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "cross"
 
 urlpatterns = [
-   path("", views.Index.as_view(), name='index'),
+    path("", views.IndexView.as_view(), name='index'),
+    path("purpose", views.PurposeView.as_view(), name='purpose'),
+    path("requirements", views.RequirementsView.as_view(), name='requirements'),
+    path("course_info", views.CourseInfoView.as_view(), name='course_info'),
+    path("student", views.StudentAreaView.as_view(), name='student'),
 ]
--- gil_website/cross_disciplinary/views.py
+++ gil_website/cross_disciplinary/views.py
@@ -1,6 +1,21 @@
 from django.views.generic import TemplateView
 
 
-class Index(TemplateView):
+class IndexView(TemplateView):
     template_name = "cross_disciplinary/index.html"
-# Create your views here.
+
+
+class PurposeView(TemplateView):
+    template_name = "cross_disciplinary/purpose.html"
+
+
+class RequirementsView(TemplateView):
+    template_name = "cross_disciplinary/requirements.html"
+
+
+class CourseInfoView(TemplateView):
+    template_name = "cross_disciplinary/course_info.html"
+
+
+class StudentAreaView(TemplateView):
+    template_name = "cross_disciplinary/student_area.html"
