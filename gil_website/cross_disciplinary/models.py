f6996c12b65585ef71d50568ce68f0d2e08770b7
richl <cclemon.lian@gmail.com>
2018-08-22 18:12:44 +0800

    changed layout

--- gil_website/cross_disciplinary/models.py
+++ gil_website/cross_disciplinary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 
 class Student(models.Model):
@@ -8,9 +9,19 @@ class Student(models.Model):
         (MALE, 'M'),
         (FEMALE, 'F')
     )
+
     student_id = models.CharField(max_length=100)
     eng_name = models.CharField(max_length=100)
     zh_name = models.CharField(max_length=100)
     gender = models.CharField(max_length=1,
                               choices=GENDER_CHOICES,)
+    college = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    reason = models.TextField()
+    primary_email = models.EmailField()
+    personal_page = models.URLField(blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    profile_picture = models.ImageField(upload_to="uploads/cross/",
+                                        blank=True)
+    more_info = models.TextField(blank=True)
 
