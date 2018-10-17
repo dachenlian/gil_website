c6c6f770d402a98883144e001a69683a12638826
dachenlian <cclemon.lian@gmail.com>
2018-10-17 08:56:16 +0800

    new student json files

--- gil_website/admissions/views.py
+++ gil_website/admissions/views.py
@@ -10,10 +10,11 @@ class IndexView(TemplateView):
 class ZhenshiView(TemplateView):
     template_name = "admissions/zhenshi.html"
     current_year = datetime.datetime.now().year
+    academic_year = current_year - 1911
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['regulations'] = "http://gra103.aca.ntu.edu.tw/gra_regular/107%E7%B0%A1%E7%AB%A0/pdf/112.pdf"
+        context['regulations'] = f"http://gra103.aca.ntu.edu.tw/gra_regular/" \
+                                 f"{self.academic_year}%E7%B0%A1%E7%AB%A0/pdf/112.pdf"
         context['hi'] = "HI"
-        # context['regulations'] = requests.get("http://gra103.aca.ntu.edu.tw/gra_regular/107%E7%B0%A1%E7%AB%A0/pdf/112.pdf").content
         return context
