36e334b7f1e386d45322b52e1f754c681bfa6a70
dachenlian <cclemon.lian@gmail.com>
2018-10-20 15:52:17 +0800

    updated faculty info

--- gil_website/people/utils.py
+++ gil_website/people/utils.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 from django.core.files import File
-from .models import MasterStudent, DoctorateStudent
+from .models import MasterStudent, DoctorateStudent, Faculty
 import json
 
 print(Path(__file__))
 STUDENT_PHOTOS = Path(__file__).parent.joinpath("static/people/gil_photos/student_photos")
+FACULTY_PHOTOS = Path(__file__).parent.joinpath("static/people/gil_photos/faculty_photos")
+CV_PATH = Path(__file__).parent.joinpath("static/people/cv")
 
 with open(Path(__file__).parent.joinpath("static/people/1071_gil_roster.json")) as fp:
     STUDENT_LIST = json.load(fp)
+
+with open(Path(__file__).parent.joinpath("static/people/gil_faculty.json")) as fp:
+    FACULTY_LIST = json.load(fp)
 
 
 def save_students_to_db(students_lst):
@@ -32,10 +37,22 @@ def save_students_to_db(students_lst):
             instance.save()
 
 
-def save_faculty_to_db(model, faculty):
-    for f in faculty:
-        query = model.objects.filter(zh_name=f['zh_name'])
+def save_faculty_to_db(faculty_lst):
+    for f in faculty_lst:
+        query = Faculty.objects.filter(zh_name=f['zh_name'])
         if query:
             continue
         else:
-            model(**f).save()
+            profile_picture = FACULTY_PHOTOS / Path(f['eng_name'].lower().replace('-', '_').replace(' ', '_'))
+            cv = CV_PATH / Path(f['eng_name'].lower().replace('-', '_').replace(' ', '_') + '_cv.pdf')
+            print(profile_picture)
+            instance = Faculty(**f)
+            try:
+                instance.profile_picture.save(profile_picture.name, File(profile_picture.with_suffix('.jpg').open('rb')))
+            except FileNotFoundError:
+                instance.profile_picture.save(profile_picture.name, File(profile_picture.with_suffix('.png').open('rb')))
+            try:
+                instance.cv_upload.save(cv.name, File(cv.open('rb')))
+            except FileNotFoundError:
+                print(f'{instance.eng_name}: CV not found.')
+            instance.save()
