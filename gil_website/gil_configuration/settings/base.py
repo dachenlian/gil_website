084d6b509e1b55544f11f98da4ab6df9132f0979
dachenlian <cclemon.lian@gmail.com>
2019-01-01 23:11:53 +0800

    removed nginx

--- gil_website/gil_configuration/settings/base.py
+++ gil_website/gil_configuration/settings/base.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,3 +142,6 @@ THUMBNAIL_ALIASES = {
 }
 
 LOGOUT_REDIRECT_URL = "/"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
