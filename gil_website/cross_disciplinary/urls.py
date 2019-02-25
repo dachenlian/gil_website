4c268cdef6699065ce2cead1cebf98e8bf3e91d4
richl <cclemon.lian@gmail.com>
2019-02-25 17:50:28 +0800

    updated cross disciplinary urls; added docker production file

--- gil_website/cross_disciplinary/urls.py
+++ gil_website/cross_disciplinary/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "cross"
 
 urlpatterns = [
-    path("index", views.IndexView.as_view(), name='index'),
-    path("purpose", views.PurposeView.as_view(), name='purpose'),
-    path("requirements", views.RequirementsView.as_view(), name='requirements'),
-    path("course_info", views.CourseInfoView.as_view(), name='course_info'),
-    path("student", views.StudentAreaView.as_view(), name='student'),
+    path("/", views.IndexView.as_view(), name='index'),
+    path("purpose/", views.PurposeView.as_view(), name='purpose'),
+    path("requirements/", views.RequirementsView.as_view(), name='requirements'),
+    path("course-info/", views.CourseInfoView.as_view(), name='course_info'),
+    path("student/", views.StudentAreaView.as_view(), name='student'),
     path("student/signup", views.SignUpView.as_view(), name='signup'),
     path("profile/<int:pk>/", views.ProfileDetailView.as_view(), name='profile_detail'),
     path("profile/<int:pk>/update", views.ProfileUpdateView.as_view(), name='profile_update'),
